a8a2ef256ec191542795e43d302b34dad12d06e1
Zain Haq <zain.haq@bookmyshow.com>
2021-02-28 04:09:28 +0530

    Add configuration file loader

--- index.js
+++ index.js
@@ -1,16 +1,20 @@
 import express from 'express';
 import request from 'request';
 
-const servers = ['http://localhost:3000', 'http://localhost:3001'];
+const { default: config } = await import(process.env.CONFIG);
+
+const port = config.PORT || 8080;
+
+const servers = config.SERVERS;
 let cur = 0;
 
 const handler = async (req, res) => {
   // Pipe the vanilla node HTTP request (a readable stream) into `request`
   // to the next server URL. Then, since `res` implements the writable stream
   // interface, you can just `pipe()` into `res`.
-  await req.pipe(request({ url: servers[cur] + req.url })).pipe(res);
+  await req.pipe(request({ url: servers[cur].URL + req.url })).pipe(res);
   cur = (cur + 1) % servers.length;
 };
 const server = express().get('*', handler).post('*', handler);
 
-server.listen(8080);
+server.listen(port, () => console.log(`Mercury running at port ${port}`));
